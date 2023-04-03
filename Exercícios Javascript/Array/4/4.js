4408b78536cf8e1231261da1cf57352282a61cca
Vitor Luiz Pestana Pereira Menezes <vitorpestanatr@gmail.com>
2023-04-03 20:18:37 -0300

    Create 4.js

---
+++ Exercícios Javascript/Array/4/4.js
@@ -0,0 +1,4 @@
+function mes(n){
+  return n == 1 ? "Janeiro" : n == 2 ? "Fevereiro" : n == 3 ? "Março" : n == 4 ? "Abril" :
+    n == 5 ? "Maio" :  n == 6 ? "Junho" : n == 7 ? "Julho" : n == 8 ? "Agosto" : n == 9 ? "Setembro" : n == 10 ? "Outubro" : n == 11 ? "Novembro" : n == 12 ? "Dezembro" : "Este número não corresponde a nenhum mês no calendário gregoriano."
+}
