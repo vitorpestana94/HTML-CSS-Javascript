6dbe2dbd75ba2fd21b756c89998406979c4b2db6
Vitor Luiz Pestana Pereira Menezes <vitorpestanatr@gmail.com>
2023-04-03 16:28:32 -0300

    Delete 2.js

--- Exercícios Javascript/Array/2.js
+++
@@ -1,7 +0,0 @@
-//Crie uma função que receba a idade de uma pessoa em anos e retorne a mesma idade em dias.
-//Create a function that receives the age of a person as year format and return the same age as day format.
-
-function anos_dias(anos){
-  return anos*365
-}
-console.log(anos_dias(10))
