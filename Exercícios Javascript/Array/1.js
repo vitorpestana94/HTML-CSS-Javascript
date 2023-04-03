578e00700e49363ee84d25388242bbaf743037d4
Vitor Luiz Pestana Pereira Menezes <vitorpestanatr@gmail.com>
2023-04-03 15:47:03 -0300

    Update 1.js

--- Exercícios Javascript/Array/1.js
+++ Exercícios Javascript/Array/1.js
@@ -1,4 +1,5 @@
 //Crie uma função que retorne um "olá" concatenado com um argumento text  e com o "!" no final
 //Create a function that returns "olá" (hello in portuguese) concatened with a text argument and with a "!" and the end of it.
+
 let ola = argumento => "Olá, ".concat(argumento, "!")
 console.log(ola("vitor"))
