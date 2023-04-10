6dcdd35fa9187fdf291fb8f2d14b19f9c7ff9ef8
Vitor Luiz Pestana Pereira Menezes <vitorpestanatr@gmail.com>
2023-04-10 19:11:47 -0300

    Update 12.js

--- Exercícios Javascript: "Curso Web Moderno Completo com JavaScript 2022 + Projetos"/Array/10-19/12/12.js
+++ Exercícios Javascript: "Curso Web Moderno Completo com JavaScript 2022 + Projetos"/Array/10-19/12/12.js
@@ -1,8 +1,7 @@
 function remover_prop(objeto, eliminar){
-  let novo_objeto = objeto
-  delete novo_objeto[eliminar]
-  return novo_objeto  
+  delete objeto[eliminar]
+  return objeto
 }
 let teste = {a:1, b:2}
 console.log(remover_prop(teste, "a"))
-console.log(Object.is(remover_prop({a:1, b:2}, "a"), teste))
+console.log(Object.is(remover_prop(teste, "a"), teste))
