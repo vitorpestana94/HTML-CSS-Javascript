12b236809309473c61f192d764182fa721650d2a
Vitor Luiz Pestana Pereira Menezes <vitorpestanatr@gmail.com>
2023-04-12 09:45:27 -0300

    Create 23.js

---
+++ Javascript/Exercícios Javascript: "Curso Web Moderno Completo com JavaScript 2022 + Projetos"/Array/20-30/23/23.js
@@ -0,0 +1,5 @@
+function palavras(string, palavras=string.split(" ")){
+  return palavras.length
+}
+console.log(palavras("Me divirto aprendendo a programar"))
+console.log(palavras("Sou uma frase"))
